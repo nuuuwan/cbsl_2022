079223dde4438fcfb52176f6f426dbb785fbe493
nuuuwan <nuuuwan@gmail.com>
2022-03-15 12:30:50 +0530

    Frequency related constants

---
+++ src/cbsl/frequency.py
@@ -0,0 +1,33 @@
+class FREQUNCY:
+    ANNUALLY = 'Annually'
+    CENSUS_YEAR = 'Census Year'
+    ACADEMIC_YEAR = 'Academic Year'
+    HALF_YEARLY = 'Half Yearly'
+    QUARTERY = 'Quarterly'
+    MONTHLY = 'Monthly'
+    DAILY = 'Daily'
+
+
+FREQUNCY_CONFIG = {
+    FREQUNCY.ANNUALLY: {
+        'dummy_value': '2022',
+    },
+    FREQUNCY.CENSUS_YEAR: {
+        'dummy_value': '2022',
+    },
+    FREQUNCY.ACADEMIC_YEAR: {
+        'dummy_value': '2022',
+    },
+    FREQUNCY.HALF_YEARLY: {
+        'dummy_value': '2022',
+    },
+    FREQUNCY.QUARTERY: {
+        'dummy_value': '2022',
+    },
+    FREQUNCY.MONTHLY: {
+        'dummy_value': '2022-01',
+    },
+    FREQUNCY.DAILY: {
+        'dummy_value': '2022-01-01',
+    },
+}
