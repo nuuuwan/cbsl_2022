6f9fa103ccb801e71be885a938db4c925882f715
nuuuwan <nuuuwan@gmail.com>
2022-03-15 14:31:29 +0530

    Use parse_page2

--- src/cbsl/pipeline.py
+++ src/cbsl/pipeline.py
@@ -5,7 +5,7 @@ from utils import jsonx
 from cbsl._constants import DIR_DATA
 from cbsl._utils import log
 from cbsl.frequency import FREQUNCY_CONFIG
-from cbsl.parsers import parse_page0, parse_page1
+from cbsl.parsers import parse_page0, parse_page1, parse_page2
 from cbsl.scrapers import (init, open_browser, open_page0, open_page1,
                            open_page2)
 
@@ -20,9 +20,15 @@ def run():
             for frequency_name in FREQUNCY_CONFIG:
                 if open_page1(browser, sub0, i_sub1, sub1, frequency_name):
                     idx1 = parse_page1(browser.page_source)
-                    idx[sub0][sub1][frequency_name] = idx1
+
                     open_page2(browser)
-                    # go_backto_page0(browser)
+                    footnote_idx = parse_page2(browser.page_source)
+                    idx1_extended = {}
+                    for sub2 in idx1:
+                        idx1_extended[sub2] = {}
+                        for sub3 in idx1[sub2]:
+                            idx1_extended[sub2][sub3] = footnote_idx[sub3]
+                    idx[sub0][sub1][frequency_name] = idx1_extended
                     break
             break
         break
