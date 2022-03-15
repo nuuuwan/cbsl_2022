89c21ba5b2f71c0f21ad9a6083d54031e8a054ce
nuuuwan <nuuuwan@gmail.com>
2022-03-15 16:44:29 +0530

    Updated go back

--- src/cbsl/pipeline.py
+++ src/cbsl/pipeline.py
@@ -6,7 +6,7 @@ from cbsl._constants import DIR_DATA
 from cbsl._utils import log
 from cbsl.frequency import FREQUNCY_CONFIG
 from cbsl.parsers import parse_page0, parse_page1, parse_page2
-from cbsl.scrapers import (init, open_browser, open_page0, open_page1,
+from cbsl.scrapers import (go_back_to_page0, go_back_to_page1, init, open_browser, open_page0, open_page1,
                            open_page2)
 
 
@@ -29,7 +29,7 @@ def save_table(sub0, sub1, frequency_name, results_idx):
                 'scale': d['scale'],
             } | d['results']
         )
-    frequency_name_str = frequency_name.lower()
+    frequency_name_str = frequency_name.replace(' ', '-').lower()
     tsv_file = os.path.join(dir, f'{frequency_name_str}.tsv')
     tsv.write(tsv_file, data_list)
     n_rows = len(data_list)
@@ -37,13 +37,24 @@ def save_table(sub0, sub1, frequency_name, results_idx):
     log.info(f'Wrote {n_rows}x{n_cols} table to {tsv_file}')
 
 
+def save_contents(idx, prefix):
+    data_file = os.path.join(DIR_DATA, f'contents.{prefix}.json')
+    jsonx.write(data_file, idx)
+    n_sub0 = len(idx)
+    log.info(f'Wrote {n_sub0} sub0s to {data_file}')
+
+
 def run():
     browser = open_browser()
     open_page0(browser)
     idx = parse_page0(browser.page_source)
 
+    save_contents(idx, 'basic')
+
     for sub0 in idx:
         for i_sub1, sub1 in enumerate(list(idx[sub0])):
+            if i_sub1 == 0:
+                continue
             for frequency_name in FREQUNCY_CONFIG:
                 if open_page1(browser, sub0, i_sub1, sub1, frequency_name):
                     idx1 = parse_page1(browser.page_source)
@@ -58,17 +69,12 @@ def run():
                             idx1_extended[sub2][sub3] = footnote_idx[sub3]
                     idx[sub0][sub1][frequency_name] = idx1_extended
 
-                    # save
                     save_table(sub0, sub1, frequency_name, results_idx)
-
+                    go_back_to_page1(browser)
+                    go_back_to_page0(browser)
                     break
-            break
-        break
 
-    data_file = os.path.join(DIR_DATA, 'contents.json')
-    jsonx.write(data_file, idx)
-    n_sub0 = len(idx)
-    log.info(f'Wrote {n_sub0} sub0s to {data_file}')
+    save_contents(idx, 'extended')
 
     browser.quit()
 
