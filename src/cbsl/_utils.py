2724cae3e466b3c4987ef8a5242da1c37088bdd3
nuuuwan <nuuuwan@gmail.com>
2022-03-16 13:33:51 +0530

    Moved get_args to utils

--- src/cbsl/_utils.py
+++ src/cbsl/_utils.py
@@ -1,5 +1,17 @@
 """Utils."""
+import argparse
 
 from utils import logx
 
 log = logx.get_logger('cbsl')
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test-mode', action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+    if args.test_mode:
+        log.warning('Running in TEST MODE')
+    else:
+        log.debug('NOT running in TEST MODE')
+    return args
