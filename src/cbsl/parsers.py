f0e7f0c080e19588a6f53112ee23d676f39190ed
nuuuwan <nuuuwan@gmail.com>
2022-03-16 10:03:50 +0530

    Added make_sub

--- src/cbsl/parsers.py
+++ src/cbsl/parsers.py
@@ -17,6 +17,15 @@ def clean(s):
     return s
 
 
+def make_sub(s, parent_s):
+    s = clean(s)
+    s = s.lower()
+    s = s.replace(' ', '-')
+    s = re.sub(r'-+', '-', s).strip()
+    s = s.replace(parent_s, '')
+    return s
+
+
 def parse_step1(html):
     log.debug('Parsing step1...')
     soup = BeautifulSoup(html, 'html.parser')
@@ -24,18 +33,18 @@ def parse_step1(html):
     n_sub1, n_sub2 = 0, 0
     for table in soup.find_all('table', {'class': CLASS_TABLE0}):
         tr_list = [tr for tr in table.find_all('tr')]
-        sub1 = clean(tr_list[0].text)
+        sub1 = make_sub(tr_list[0].text)
         n_sub1 += 1
         idx[sub1] = {}
         for tr in tr_list[1:]:
-            sub2 = clean(tr.find_all('td')[1].text)
+            sub2 = make_sub(tr.find_all('td')[1].text)
             idx[sub1][sub2] = {}
             n_sub2 += 1
     log.info(f'Found {n_sub1} sub1s, {n_sub2} sub2s')
     return idx
 
 
-def parse_step2(html):
+def parse_step2(html, sub2):
     log.debug('Parsing step2...')
     soup = BeautifulSoup(html, 'html.parser')
     idx = {}
@@ -47,12 +56,12 @@ def parse_step2(html):
                 continue
             td = td_list[2]
             if 'GdHDColor1' in td.attrs['class']:
-                sub3 = clean(td.text)
+                sub3 = make_sub(td.text, sub2)
                 idx[sub3] = {}
                 n_sub3 += 1
 
             else:
-                sub4 = clean(td.text)
+                sub4 = make_sub(td.text, sub3)
                 if sub4:
                     idx[sub3][sub4] = {}
                     n_sub4 += 1
