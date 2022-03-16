516ac9e30108c7f6aa467ad0b6fd6c718147c868
nuuuwan <nuuuwan@gmail.com>
2022-03-16 09:42:30 +0530

    Updates

--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -1,10 +1,15 @@
 git reset --hard HEAD
 
 git checkout data
+git pull origin data
 
+ls -la
 rm -rf *
 rm -rf .github
+ls -la
 
 git add .
-git commit -m "[clean_data] cleaned"
+git commit -m "[clean_data] $(date)"
 git push origin data
+
+git checkout master
