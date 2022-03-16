3d64f24e7cb456428516b22f14a123cba72bf358
nuuuwan <nuuuwan@gmail.com>
2022-03-16 11:20:27 +0530

    Robust mkdir

--- src/cbsl/core/data.py
+++ src/cbsl/core/data.py
@@ -11,7 +11,7 @@ IDX2_FILE = os.path.join(DIR_DATA, 'contents.basic.json')
 def git_checkout():
     if os.path.exists(DIR_DATA):
         os.system(f'rm -rf {DIR_DATA}')
-    os.mkdir(DIR_DATA)
+    os.system(f'mkdir -p {DIR_DATA}')
 
     os.system(';'.join([
         f'cd {DIR_DATA}',
